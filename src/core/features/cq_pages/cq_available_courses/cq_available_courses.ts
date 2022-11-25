c33235747a98fcabc93ddac1b4be9ac81b782942
Rachmad Herdiyanto <rachmadherdiyanto@cyberquote.co.id>
2022-11-25 17:16:40 +0700

    CQ Available Courses

--- src/core/features/cq_pages/cq_available_courses/cq_available_courses.ts
+++ src/core/features/cq_pages/cq_available_courses/cq_available_courses.ts
@@ -1,6 +1,7 @@
 // done v3
 
 import { Component, ViewChild, Renderer2, OnInit } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 import { CqHelper } from '../services/cq_helper';
 import { CqPage } from '../classes/cq_page';
 // import { CqFilterComponent } from '../components/cq-filter/cq-filter';
@@ -13,22 +14,33 @@ import { CqPage } from '../classes/cq_page';
 export class CqAvailableCourses extends CqPage implements OnInit
 {
     // @ViewChild(CqFilterComponent) filter: CqFilterComponent;
+    @ViewChild('pageSlider', { static: true }) private pageSlider: IonSlides;
 
     pageParams = {
     };
     pageDefaults: any = {
-        filterMultiple: [],
-        courses: [],
-        coursesFiltered: [],
-    };
-    pageJob: any = {
-        filterMultiple: {
-            value: 0,
-            next: {
-                availableCourses: 0,
-            }
+        medias: ["online", "offline"],
+        media: "online",
+        online: {
+            initiated: false,
+            courses: [],
+            filterMultiple: [],
+            filterAgent: null,
+            filterText: "",
+        },
+        offline: {
+            initiated: false,
+            courses: [],
+            filterMultiple: [],
+            filterAgent: null,
+            filterText: "",
         },
     };
+    pageJob: any = {
+        filterMultiple: 0,
+        courses: 0,
+    };
+    // warning! harus bisa bedain online dan offline
     pageJobLoadMore: any = {
         courses: 0,
     };
@@ -36,6 +48,15 @@ export class CqAvailableCourses extends CqPage implements OnInit
     constructor(renderer: Renderer2, CH: CqHelper)
     {
         super(renderer, CH);
+
+        this.pageData.pageSliderOptions = {
+            initialSlide: 0,
+            speed: 400,
+            centerInsufficientSlides: true,
+            centeredSlides: true,
+            centeredSlidesBounds: true,
+            slidesPerView: 1,
+        };
     }
 
     ngOnInit(): void { this.usuallyOnInit(); }
@@ -47,80 +68,124 @@ export class CqAvailableCourses extends CqPage implements OnInit
     filterMultiple(jobName: string, moreloader?: any, refresher?: any, modeData?: any, nextFunction?: any, finalCallback?: any): void
     {
         const params: any = {
-            class: 'CqLib',
-            function: 'get_filter_multiple',
-            page: 'my_courses_list',
-        };
-
-        this.pageJobExecuter(jobName, params, (data) => {
-            this.pageData.filterMultiple = this.CH.toJson(data);
-
-            if (typeof nextFunction == 'function') nextFunction(jobName, moreloader, refresher, finalCallback);
-        }, moreloader, refresher, finalCallback);
-    }
-    availableCourses(jobName: string, moreloader?: any, refresher?: any, modeData?: any, nextFunction?: any, finalCallback?: any): void
-    {
-        const params: any = {
             calls: {
-                onlineCourses: {
-                    class: "CqCourseLib",
-                    function: "get_e_learning_list",
-                    page: modeData.page,
-                    length: modeData.length,
+                onlineFilter: {
+                    class: 'CqLib',
+                    function: 'get_filter_multiple',
+                    page: 'e_learning_list',
                 },
-                offlineCourses: {
-                    class: "CqCourseLib",
-                    function: "get_classroom_training_list",
-                    page: modeData.page,
-                    length: modeData.length,
+                offlineFilter: {
+                    class: 'CqLib',
+                    function: 'get_filter_multiple',
+                    page: 'classroom_training_list',
                 },
             },
         };
 
         this.pageJobExecuter(jobName, params, (data) => {
-            let allCourses = this.CH.toJson(data),
-                callId, courses;
+            let allData = this.CH.toJson(data);
 
-            for (callId in allCourses)
-            {
-                if (callId == "onlineCourses")
-                {
-                    courses = allCourses[callId];
-                    this.reachedEndOfList = this.CH.isEmpty(courses) || this.CH.getLength(courses) < modeData.length;
-                    if (this.CH.isEmpty(courses)) return;
+            // onlineFilter
+            this.pageData.online.filterMultiple = allData.onlineFilter;
 
-                    if (modeData.mode == 'firstload') this.pageData.courses = courses;
-                    else if (modeData.mode == 'loadingmore') this.pageData.courses = this.pageData.courses.concat(courses);
-                    else if (modeData.mode == 'refreshing') this.pageData.courses = courses;
-                    // this.pageData.coursesFiltered = this.filter.getFilteredData(this.pageData.courses);
-                }
-                else if (callId == "offlineCourses")
-                {
-                    courses = allCourses[callId];
-                    this.reachedEndOfList = this.CH.isEmpty(courses) || this.CH.getLength(courses) < modeData.length;
-                    if (this.CH.isEmpty(courses)) return;
+            // offlineFilter
+            this.pageData.offline.filterMultiple = allData.offlineFilter;
 
-                    // aditional information
-                    for (let id in courses)
-                    {
-                        courses[id].letter = "A"; //courses[id].name.substr(0, 1);
-                        courses[id].venue = courses[id].venue ? courses[id].venue : '-';
-                        courses[id].media = 'offline';
-                    }
-
-                    if (modeData.mode == 'firstload') this.pageData.courses = courses;
-                    else if (modeData.mode == 'loadingmore') this.pageData.courses = this.pageData.courses.concat(courses);
-                    else if (modeData.mode == 'refreshing') this.pageData.courses = courses;
-                    // this.pageData.coursesFiltered = this.filter.getFilteredData(this.pageData.courses);
-                    this.pageData.coursesFiltered = this.pageData.courses;
-                }
-            }
+            if (typeof nextFunction == 'function') nextFunction(jobName, moreloader, refresher, finalCallback);
         }, moreloader, refresher, finalCallback);
     }
-
-    onFilterChange(): void
+    courses(jobName: string, moreloader?: any, refresher?: any, modeData?: any, nextFunction?: any, finalCallback?: any): void
     {
-        // this.pageData.coursesFiltered = this.filter.getFilteredData(this.pageData.courses);
-        this.pageData.coursesFiltered = this.pageData.courses;
+        if (this.pageData.media == "online")
+        {
+            // warning! modeData itu bisa bingung antara dua slide
+            const params: any = {
+                class: "CqCourseLib",
+                function: "get_e_learning_list",
+                page: modeData.page,
+                length: modeData.length,
+                search: this.pageData.online.filterText,
+            };
+
+            this.pageJobExecuter(jobName, params, (data) => {
+                let courses = this.CH.toArray(this.CH.toJson(data));
+                this.pageData.online.initiated = true;
+                this.pageData.online.reachedEndOfList = this.reachedEndOfList = this.CH.isEmpty(courses) || this.CH.getLength(courses) < modeData.length;
+
+                if (modeData.mode != 'loadmore') this.pageData.online.courses = courses;
+                else this.pageData.courses = this.pageData.online.courses.concat(courses);
+
+                if (typeof nextFunction == 'function') nextFunction(jobName, moreloader, refresher, finalCallback);
+            }, moreloader, refresher, finalCallback);
+        }
+        else if (this.pageData.media == "offline")
+        {
+            // warning! modeData itu bisa bingung antara dua slide
+            const params: any = {
+                class: "CqCourseLib",
+                function: "get_classroom_training_list",
+                page: modeData.page,
+                length: modeData.length,
+                search: this.pageData.offline.filterText,
+            };
+
+            this.pageJobExecuter(jobName, params, (data) => {
+                let courses = this.CH.toArray(this.CH.toJson(data));
+                this.pageData.offline.initiated = true;
+                this.pageData.offline.reachedEndOfList = this.reachedEndOfList = this.CH.isEmpty(courses) || this.CH.getLength(courses) < modeData.length;
+
+                // aditional information
+                for (let id in courses)
+                {
+                    courses[id].letter = courses[id].name.substr(0, 1);
+                    courses[id].venue = courses[id].venue ? courses[id].venue : '-';
+                    courses[id].media = 'offline';
+                }
+
+                if (modeData.mode != 'loadmore') this.pageData.offline.courses = courses;
+                else this.pageData.courses = this.pageData.offline.courses.concat(courses);
+
+                if (typeof nextFunction == 'function') nextFunction(jobName, moreloader, refresher, finalCallback);
+            }, moreloader, refresher, finalCallback);
+        }
+    }
+
+    selectMedia(media): void
+    {
+        if (this.pageData.media != media)
+        {
+            this.pageData.media = media;
+            let mediaIndex = this.pageData.medias.indexOf(this.pageData.media);
+            this.pageSlider.slideTo(mediaIndex);
+            if (!this.pageData[this.pageData.media].initiated) this.pageForceReferesh();
+        }
+    }
+    pageSliderChange(): void
+    {
+        if (this.pageStatus)
+        {
+            this.pageSlider.getActiveIndex().then((index) => {
+                this.pageData.media = this.pageData.medias[index];
+                if (!this.pageData[this.pageData.media].initiated) this.pageForceReferesh();
+                else this.CH.log('final data', this.pageData);
+            });
+        }
+    }
+    onFilterChange(data: any): void
+    {
+        this.pageIsLoading = true;
+        clearTimeout(this.pageData[this.pageData.media].filterAgent);
+        let locaAgent = this.pageData[this.pageData.media].filterAgent = setTimeout(() => {
+            let text = data.text.trim().toLowerCase();
+
+            if (locaAgent != this.pageData[this.pageData.media].filterAgent || text == this.pageData[this.pageData.media].filterText)
+            {
+                this.pageIsLoading = false;
+                return;
+            }
+
+            this.pageData[this.pageData.media].filterText = data.text;
+            this.pageForceReferesh();
+        }, 1000);
     }
 }

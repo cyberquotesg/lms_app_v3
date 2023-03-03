25e7009c121ec04a69b0947e7cde95278091f16c
Rachmad Herdiyanto <rachmadherdiyanto@cyberquote.co.id>
2023-03-03 13:41:53 +0700

    CQ Items | Fix Bug on Subtitle

--- src/core/features/cq_pages/components/cq_items/cq_items.ts
+++ src/core/features/cq_pages/components/cq_items/cq_items.ts
@@ -55,7 +55,7 @@ export class CqItemsComponent extends CqComponent implements OnInit, OnChanges {
         this.items.forEach((item) => {
             item.title = item.title || item.subject || item.name || item.displayname || item.fullname_trimmed || item.fullnameTrimmed || item.fullname || item.shortname || "";
             item.letter = this.CH.getLetter(item.title) || "";
-            item.subTitle = item.subTitle || item.description || item.summary || item.smallmessage || item.timecreated || "";
+            item.subTitle = (item.subTitle || item.description || item.summary || item.smallmessage || item.timecreated || "").replace(/\&lt;/g, "<").replace(/\&gt;/g, ">");
             item.image = item.image || item.course_image_full || item.courseImageFull || item.course_image || item.courseImage ||  item.thumbnail || item.iconurl || item.profileimageurlfrom || null;
 
             if (item.has_finished || item.hasFinished)
